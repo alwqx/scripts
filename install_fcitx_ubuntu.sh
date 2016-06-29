7e2d0afbdf1f4647c3b7f6c4a64586272d6d1489
adolphlwq <kenan3015@gmail.com>
2016-06-29 10:10:40 +0800

    add installing fcitx

---
+++ install_fcitx_ubuntu.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+###
+# 1. install fcitx
+# 2. must reboot system
+# 3. config language and input method
+# 4. reference: http://jingyan.baidu.com/article/4b07be3c60da3f48b380f3f0.html
+###
+
+sudo apt-get install fcitx-googlepinyin fcitx-pinyin
