44b96798d82a52cb00f50638239aeb692eafc325
adolphlwq <kenan3015@gmail.com>
2016-08-28 03:37:11 +0800

    add commont

--- ghost_blog_data_backup.sh
+++ ghost_blog_data_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DATA_DIR=$1
-STORE_DIR=$2
+DATA_DIR=$1    #data dir to backup
+STORE_DIR=$2   #data dir to store backup date
 tar zcvf ${STORE_DIR}/ghost_content_data_`date "+%Y_%m_%d_%H_%M_%S"`.tar.gz ${DATA_DIR}
