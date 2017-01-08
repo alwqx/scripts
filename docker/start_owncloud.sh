86d3a5638840a56e032f4d04789e58091d9cf7b6
adolphlwq <kenan3015@gmail.com>
2017-01-08 22:50:57 +0800

    update owncloud

--- docker/start_owncloud.sh
+++ docker/start_owncloud.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
-docker run -d --name owncloud -p 9090:80 owncloud:8.1
+docker run -d --name owncloud \
+-p 9090:80 \
+--link postgres:postgres \
+--restart always \
+-v `pwd`/data:/var/www/html \
+owncloud:9.1.3
