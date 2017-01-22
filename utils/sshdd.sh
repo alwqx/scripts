fb66adb801f9bc00d8d61d133b1f9f9778db3b52
Adolphlwq <kenan3015@gmail.com>
2017-01-22 14:10:07 +0800

    add reference

--- utils/sshdd.sh
+++ utils/sshdd.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
+# reference: http://www.linuxweblog.com/node/282
+
 echo "below is just an example,please change port, user, ip and remote_ssh_port"
 echo "ssh -N -f -C -g -R port:localhost:22 user@ip -p remote_ssh_port"
