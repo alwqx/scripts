96fe8f30b845ae95d1bdeaf2c31ea5e629bc41f9
adolphlwq <kenan3015@gmail.com>
2017-03-06 10:42:06 +0800

    fix #1

--- docker/rm_container.sh
+++ docker/rm_container.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
-docker ps -a | grep Exited | awk '{print $1}' | xargs docker rm
+exited_containers=$(docker ps -a | grep Exited | wc -l)
+if [[ ${exited_containers=} -eq 0 ]];then
+    echo "no exited docker container."
+else
+    docker ps -a | grep Exited | awk '{print $1}' | xargs docker rm
+fi
