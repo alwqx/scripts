ebb4c914c110a66320450dedc1975563573eddbf
Adolphlwq <kenan3015@gmail.com>
2017-04-05 10:24:54 +0800

    remove =

--- docker/rm_container.sh
+++ docker/rm_container.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 exited_containers=$(docker ps -a | grep Exited | wc -l)
-if [[ ${exited_containers=} -eq 0 ]];then
+if [[ ${exited_containers} -eq 0 ]];then
     echo "no exited docker container."
 else
     docker ps -a | grep Exited | awk '{print $1}' | xargs docker rm
